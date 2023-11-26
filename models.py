4cb13819e334fd27acafd9242dc0b7949b197e12
Diego Delgado Carrillo <diego@Diegos-MacBook-Air.local>
2023-11-25 19:59:55 -0700

    part 2

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -10,3 +12,24 @@ class User(db.Model):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+    
+def connect_db(app):
+    
+    db.app = app
+    db.init_app(app)
